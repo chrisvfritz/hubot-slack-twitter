87560eec0d938a75a57301dbd862c2a07adcd18b
Chris Fritz <chrisvfritz@gmail.com>
2015-09-02 17:27:59 -0400

    manually added default config to hubot-slack-twitter

--- library/fileUtils.js
+++ library/fileUtils.js
@@ -8,7 +8,19 @@ var path = require('path');
 
 module.exports = {
   getStorageFile: function(name, defaultContent) {
-    defaultContent = defaultContent || {};
+    defaultContent = defaultContent || {
+      "accounts": {
+        "lansingcodes": {
+          "consumer_key": process.env.TWITTER_LANSINGCODES_CONSUMER_KEY,
+          "consumer_secret": process.env.TWITTER_LANSINGCODES_CONSUMER_SECRET,
+          "access_token": process.env.TWITTER_LANSINGCODES_ACCESS_TOKEN,
+          "access_token_secret": process.env.TWITTER_LANSINGCODES_ACCESS_TOKEN_SECRET
+        }
+      },
+      "rooms": {
+        "twitter": "lansingcodes"
+      }
+    };
     var pth = path.resolve(__dirname, './../../../' + filePrefix + name + '.json');
     return (fs.existsSync(pth) ? require(pth) : defaultContent);
   },
